de9b9782dc482f1116bcc8caef48f37ecaa5853b
mzDAN <mh.ziaei.dan@gmail.com>
2020-09-29 17:00:14 +0330

    replacing class bese view with TemplateView instead of function view

--- my_library/urls.py
+++ my_library/urls.py
@@ -5,5 +5,5 @@ from . import views
 # set the 'app_name' to namespaceing in urls!!!
 app_name = 'my_library'
 urlpatterns = [
-    path("", views.index, name="index"),
+    path("", views.IndexView.as_view(), name="index"),
 ]
--- my_library/views.py
+++ my_library/views.py
@@ -1,22 +1,35 @@
 from django.shortcuts import render
+from django.views import generic
 
 from . import models
 
 
-def index(request):
-    """
-    View function for home page of site.
-    """
-    # Generate counts of some of the main objects
-    num_books = models.Book.objects.all().count()
-    num_instances = models.BookInstance.objects.all().count()
-    # Available books (status = 'a')
-    num_instances_available = models.BookInstance.objects.filter(status__exact='a'
-                                                                 ).count()
-    # The 'all()' is implied by default.
-    num_authors = models.Author.objects.all().count()
+# def index(request):
+#     """
+#     View function for home page of site.
+#     """
+#     # Generate counts of some of the main objects
+#     num_books = models.Book.objects.all().count()
+#     num_instances = models.BookInstance.objects.all().count()
+#     # Available books (status = 'a')
+#     num_instances_available = models.BookInstance.objects.filter(status__exact='a'
+#                                                                  ).count()
+#     # The 'all()' is implied by default.
+#     num_authors = models.Author.objects.all().count()
+#
+#     # Render the HTML template index.html with the data in the context variable
+#     return render(request, 'my_library/index.html',
+#                   context={'num_books': num_books, 'num_instances': num_instances,
+#                            'num_instances_available': num_instances_available, 'num_authors': num_authors})
+#
+class IndexView(generic.TemplateView):
+    template_name = "my_library/index.html"
 
-    # Render the HTML template index.html with the data in the context variable
-    return render(request, 'my_library/index.html',
-                  context={'num_books': num_books, 'num_instances': num_instances,
-                           'num_instances_available': num_instances_available, 'num_authors': num_authors})
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["num_books"] = models.Book.objects.all().count()
+        context["num_instances"] = models.BookInstance.objects.all().count()
+        context["num_instances_available"] = models.BookInstance.objects.filter(
+            status__exact='a').count()
+        context["num_authors"] = models.Author.objects.all().count()
+        return context
